780b7fe7bbcdec82f4a59ef4f36c3fb44a6669e5
sina <sina.tabriziyan@gmail.com>
2023-11-17 04:44:42 -0800

    1-[database] implement another query in Dao

--- app/src/main/java/com/sina/fullnotecompose/data/TodoDao.kt
+++ app/src/main/java/com/sina/fullnotecompose/data/TodoDao.kt
@@ -32,4 +32,9 @@ interface TodoDao {
 
     @Query("select * from todo_table where title like :searchQuery or description like :searchQuery")
     fun searchDatabase(searchQuery: String): Flow<List<ToDoTask>>
+
+    @Query("select * from todo_table order by case when priority like 'L%' then 1 when priority like 'M%' then 2 when priority like 'H%' then 3 end")
+    fun sortByLowPriority():Flow<List<ToDoTask>>
+    @Query("select * from todo_table order by case when priority like 'H%' then 1 when priority like 'M%' then 2 when priority like 'L%' then 3 end")
+    fun sortByHighPriority():Flow<List<ToDoTask>>
 }
